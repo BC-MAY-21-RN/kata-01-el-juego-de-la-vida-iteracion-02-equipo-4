2365fd83e8fbe0e59f66eb757c5160243cff8ab5
Ayeza Elizabeth Zamudio Comino <ayezahmvc14@gmail.com>
2022-05-04 16:23:27 -0500

    tercer commit

--- img/script.js
+++ img/script.js
@@ -5,10 +5,14 @@ class Tablero {
   }
   crear_tablero(fila, columna) {
     var matriz = new Array();
-    for (let x = 0; x < fila; x++) 
-    {
-        matriz[x] = new Array();
-        
+    for (let x = 0; x < fila; x++) {
+      matriz[x] = new Array();
+      for (let y = 0; y < columnas; y++) {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.9) {
+          matriz[x][y] = "-";
+        }
+      }
     }
   }
 }
